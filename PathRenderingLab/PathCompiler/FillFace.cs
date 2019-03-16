feecb2b09d7312219afd54c882840c0f1f14a93b
João Baptista de Paula e Silva <jbaptistapsilva@yahoo.com.br>
2019-03-16 15:56:46 +0100

    Change the curve subdivision algorithms

--- PathRenderingLab/PathCompiler/FillFace.cs
+++ PathRenderingLab/PathCompiler/FillFace.cs
@@ -85,17 +85,10 @@ namespace PathRenderingLab.PathCompiler
             var contourLists = Contours.Select(c => new LinkedList<Curve>(c)).ToArray();
 
             // Divide the curves into convex and concave curves
-            var convexCurves = new LinkedList<LinkedListNode<Curve>>();
-            var concaveCurves = new LinkedList<LinkedListNode<Curve>>();
+            var curves = new LinkedList<LinkedListNode<Curve>>();
             foreach (var contour in contourLists)
                 for (var cnode = contour.First; cnode != null; cnode = cnode.Next)
-                {
-                    var curve = cnode.Value;
-
-                    // For the sake of this test, lines are considered concave curves
-                    if (curve.Type != CurveType.Line && curve.IsConvex) convexCurves.AddLast(cnode);
-                    else concaveCurves.AddLast(cnode);
-                }
+                    curves.AddLast(cnode);
 
             bool subdivide = true;
             // Iterate for each convex-concave pair until no subdivisions are needed anymore
@@ -103,160 +96,61 @@ namespace PathRenderingLab.PathCompiler
             {
                 subdivide = false;
 
-                TestListSubdivision(convexCurves, concaveCurves);
-                TestListSubdivision(convexCurves, convexCurves);
-
-                void TestListSubdivision(LinkedList<LinkedListNode<Curve>> list1, LinkedList<LinkedListNode<Curve>> list2)
+                for (var node1 = curves.First; node1 != null; node1 = node1.Next)
                 {
-                    for (var onode = list1.First; onode != null; onode = onode.Next)
-                    {
-                        // Skip degenerate curves
-                        if (onode.Value.Value.IsDegenerate) continue;
-
-                        for (var inode = list2.First; inode != null; inode = inode.Next)
-                        {
-                            // Skip equal curves
-                            if (inode.Value.Value == onode.Value.Value) continue;
-
-                            // Skip degenerate curves
-                            if (inode.Value.Value.IsDegenerate) continue;
-
-                            // A function for subdividing the curves
-                            void SubdivideCurveInHalf(LinkedListNode<LinkedListNode<Curve>> node)
-                            {
-                                var curve = node.Value.Value;
-                                node.List.AddAfter(node, node.Value.List.AddAfter(node.Value, curve.Subcurve(0.5, 1)));
-                                node.Value.Value = curve.Subcurve(0, 0.5);
-                            }
-
-                            while (true)
-                            {
-                                // Get the curve intersection info for the pair
-                                var intersectionInfo = GetIntersectionInfoFromCurves(onode.Value.Value, inode.Value.Value);
-
-                                // If there is no intersection, bail out
-                                if (intersectionInfo == null) break;
-                                subdivide = true;
-
-                                // Get the case switches for it
-                                var interiorPoints1 = intersectionInfo.InteriorPoints1;
-                                var interiorPoints2 = intersectionInfo.InteriorPoints2;
-                                var l1 = interiorPoints1.Length > 0;          // Polygon 1 has points from polygon 2 in its interior
-                                var l2 = interiorPoints2.Length > 0;          // Polygon 2 has points from polygon 1 in its interior
-                                var k1 = intersectionInfo.CurveIntersects1;   // Curve 1 intersects polygon 2
-                                var k2 = intersectionInfo.CurveIntersects2;   // Curve 2 intersects polygon 1
-
-                                if ((!l1 && !l2 && !k1 && !k2) || (l1 && l2) || (k1 && k2))
-                                {
-                                    SubdivideCurveInHalf(onode);
-                                    SubdivideCurveInHalf(inode);
-                                }
-                                else if (l1 && !k1) SubdivideCurveInHalf(onode);
-                                else if (l2 && !k2) SubdivideCurveInHalf(inode);
-                                else if (k1) SubdivideCurveInHalf(inode);
-                                else if (k2) SubdivideCurveInHalf(onode);
-                                else Debug.Assert(false, "This should not happen!");
-
-                                // If we generate degenerate curves, bail out too
-                                if (onode.Value.Value.IsDegenerate || inode.Value.Value.IsDegenerate) break;
-                            }
-                        }
-                    }
-                }
-            }
-
-            // Now, we are going to compare concave with line curves
-            // Remove line curves from the concave curves and populate a list with the line curves
-            var lineCurves = new LinkedList<LinkedListNode<Curve>>();
-            for (var inode = concaveCurves.First; inode != null;)
-            {
-                var next = inode.Next;
-
-                if (inode.Value.Value.Type == CurveType.Line)
-                {
-                    inode.List.Remove(inode.Value);
-                    lineCurves.AddLast(inode.Value);
-                }
-
-                inode = next;
-            }
-
-            subdivide = true;
-            // Iterate for each line-concave pair until no subdivisions are needed anymore
-            while (subdivide)
-            {
-                subdivide = false;
-
-                for (var lnode = lineCurves.First; lnode != null; lnode = lnode.Next)
-                {
-                    var line = lnode.Value.Value;
-                    var a = line.A;
-                    var b = line.B;
-
                     // Skip degenerate curves
-                    if (line.IsDegenerate) continue;
+                    if (node1.Value.Value.IsDegenerate) continue;
 
-                    for (var inode = concaveCurves.First; inode != null; inode = inode.Next)
+                    for (var node2 = curves.First; node2 != null; node2 = node2.Next)
                     {
-                        retry: // Return-to label if the concave curve needs to be subdivided further 
-                        var concave = inode.Value.Value;
-                        var ccPoly = concave.EnclosingPolygon;
+                        // Skip equal curves
+                        if (node2.Value.Value == node1.Value.Value) continue;
 
                         // Skip degenerate curves
-                        if (concave.IsDegenerate) continue;
+                        if (node2.Value.Value.IsDegenerate) continue;
 
-                        // Check first if the line's endpoints don't coincide with the curve's
-                        // If they are, there is nothing left to do
-                        if (DoubleUtils.RoughlyEquals(concave.At(0), b) ||
-                            DoubleUtils.RoughlyEquals(concave.At(1), a)) continue;
+                        // Skip curves that are eligible to double curve
+                        if (AreCurvesFusable(node1.Value.Value, node2.Value.Value)) continue;
 
-                        // Now, for each vertex, check if is ouside the line
-                        foreach (var v in ccPoly)
+                        // A function for subdividing the curves
+                        void SubdivideCurveInHalf(LinkedListNode<LinkedListNode<Curve>> node)
                         {
-                            // Get the nearest point
-                            double t = concave.NearestPointTo(v);
-                            var p = concave.At(t);
-
-                            // If at the endpoints, no need to subdivide it
-                            if (DoubleUtils.RoughlyZero(t) || DoubleUtils.RoughlyEquals(t, 1)) continue;
-
-                            // If the line connecting both intersects the original line, subdivide right there
-                            if (GeometricUtils.SegmentsIntersect(a, b, p, v))
-                            {
-                                subdivide = true;
-
-                                var c1 = concave.Subcurve(0, t);
-                                var c2 = concave.Subcurve(t, 1);
-
-                                // Update the linked list
-                                inode.List.AddBefore(inode, inode.Value.List.AddBefore(inode.Value, c1));
-                                inode.Value.Value = c2;
-
-                                // Retry this iteration with the newly-divided curve
-                                goto retry;
-                            }
+                            var curve = node.Value.Value;
+                            node.List.AddAfter(node, node.Value.List.AddAfter(node.Value, curve.Subcurve(0.5, 1)));
+                            node.Value.Value = curve.Subcurve(0, 0.5);
                         }
 
-                        // Finnaly, we check if any point is inside the polygon
-                        foreach (var v in new[] { a, b })
-                            if (GeometricUtils.PolygonContainsPoint(ccPoly, v))
+                        while (true)
+                        {
+                            // Get the curve intersection info for the pair
+                            var intersectionInfo = GetIntersectionInfoFromCurves(node1.Value.Value, node2.Value.Value);
+
+                            // If there is no intersection, bail out
+                            if (intersectionInfo == null) break;
+                            subdivide = true;
+
+                            // Get the case switches for it
+                            var interiorPoints1 = intersectionInfo.InteriorPoints1;
+                            var interiorPoints2 = intersectionInfo.InteriorPoints2;
+                            var l1 = interiorPoints1.Length > 0;          // Polygon 1 has points from polygon 2 in its interior
+                            var l2 = interiorPoints2.Length > 0;          // Polygon 2 has points from polygon 1 in its interior
+                            var k1 = intersectionInfo.CurveIntersects1;   // Curve 1 intersects polygon 2
+                            var k2 = intersectionInfo.CurveIntersects2;   // Curve 2 intersects polygon 1
+
+                            if ((!l1 && !l2 && !k1 && !k2) || (l1 && l2) || (k1 && k2))
                             {
-                                // If the nearest point is one of the endpoints, no need to subdivide it
-                                double t = concave.NearestPointTo(v);
-                                if (DoubleUtils.RoughlyZero(t) || DoubleUtils.RoughlyEquals(t, 1)) continue;
-
-                                subdivide = true;
-
-                                var c1 = concave.Subcurve(0, t);
-                                var c2 = concave.Subcurve(t, 1);
-
-                                // Update the linked list
-                                inode.List.AddBefore(inode, inode.Value.List.AddBefore(inode.Value, c1));
-                                inode.Value.Value = c2;
-
-                                // Retry this iteration with the newly-divided curve
-                                goto retry;
+                                SubdivideCurveInHalf(node1);
+                                SubdivideCurveInHalf(node2);
                             }
+                            else if (l1 && !k1) SubdivideCurveInHalf(node1);
+                            else if (l2 && !k2) SubdivideCurveInHalf(node2);
+                            else if (k1) SubdivideCurveInHalf(node2);
+                            else if (k2) SubdivideCurveInHalf(node1);
+                            else Debug.Assert(false, "This should not happen!");
+
+                            // If we generate degenerate curves, bail out too
+                            if (node1.Value.Value.IsDegenerate || node2.Value.Value.IsDegenerate) break;
+                        }
                     }
                 }
             }
